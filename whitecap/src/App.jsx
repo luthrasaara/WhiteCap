f1d59eaec57f376e4f495ef9f578fddc220b23a6
Saara <s.saara@mail.utoronto.ca>
2026-02-10 13:05:38 -0500

    home page

--- whitecap/src/App.jsx
+++ whitecap/src/App.jsx
@@ -2,54 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import msLogo from './assets/ml.png';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-		<div id = "container">
-			<div id = "box">
-				<div id = "signInOptions">
-					<button className = "activeButton">User</button>
-					<button>Admin</button>
-				</div>
-			
-				<h1>Welcome Back</h1>
-				<h3>Sign in with your Microsoft Entra ID</h3>
-				
-				<div id = "inputField">
-					<h4>Username</h4>
-					<input className = "inputBox" placeholder = "Enter your username"/>
-				</div>
-
-				<div id = "inputField">			
-					<h4>Password</h4>
-					<input className = "inputBox" placeholder = "Enter your password"/>
-				</div>
-				
-				<div id = "stuff">
-					<div id = "rmbr">
-						<input type = "checkbox"/>
-						<span>Remember me</span>
-					</div>
-					<div id = "forgot">
-						<a href = "#"><span>Forgot password?</span></a>
-					</div>
-				</div>
-				
-				<button id = "signInButton">Sign in as User</button>
-				
-				<div id = "noAccount">
-					<span>
-						Dont have an account? 
-						<a href = "#"> Sign Up</a>
-					</span>
-				</div>
+		<header className="navbar">
+			<div className="nav-left">
+				<span className="text">WhiteCap</span>
 			</div>
+
+			<div className="nav-right">
+				<button className="sign-in">
+					<img src={msLogo} alt="Microsoft" className="logo" />
+					Sign In
+				</button>
+			</div>
+		</header>
+
+			
+		<div className="hero">
+			<h1>Welcome</h1>
+
+			<button>
+				<img src={msLogo} alt="Microsoft" className="btn-logo" />
+				Get Started
+			</button>
 		</div>
-    </>
-  )
+	
+	</>
+	
+		
+    
+  );
 }
 
 export default App
--- whitecap/src/main.jsx
+++ whitecap/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "@fontsource/inter/100.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
